ee23dc91910d6926ddb8b261bb478864a5e7d079
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-02-22 14:25:52 -0300

    Adcionando opção de reset

--- js/js.js
+++ js/js.js
@@ -1,11 +1,23 @@
 var res1 = window.document.getElementById('res1');
 var res2 = window.document.getElementById('res2');
+var btn1 = window.document.getElementById('btn1');
+var btn2 = window.document.getElementById('btn2');
 
 function click1(){
     var player  = window.prompt("Digite o nome do jogador: ");
     if(player == "cassio"){
-        console.log("certo");
         res1.innerHTML = ("<img src=/imgs/cassio.png id='foto'>")
         res2.innerHTML = ("Cássio Ramos!");
     }
+    else{
+        window.alert("Por favor verifique se o nome digitado está correto!");
+    }
+    btn1.style.display = 'none';
+    btn2.style.display = 'inline';
+}
+function click2(){
+    res1.innerHTML = ("");
+    res2.innerHTML = ("");
+    btn2.style.display = 'none';
+    btn1.style.display = 'inline';
 }
